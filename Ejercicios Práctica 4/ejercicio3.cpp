fe5977b6bc482b88890fede306df0d62e17e8b55
KevinEsc21 <62626446+KevinEsc21@users.noreply.github.com>
2020-05-26 16:29:36 -0600

    TAREA 4

---
+++ Ejercicios Práctica 4/ejercicio3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int n; 
+
+    cout << endl << "PROGRAMA QUE VERIFICA SI UN NUMERO ES POSITIVO, NEGATIVO O CERO"<< endl <<endl;
+
+    cout << "Inserte el numero a verificar: ";
+    cin >> n;
+    cout << endl;
+
+    if (n > 0){
+        cout << n << " es positivo";
+    }else if (n < 0) {
+        cout << n << " es negativo"; 
+    }else {
+        cout << "El numero es cero";
+    }
+
+    cout << endl << endl;
+
+    return 0;
+}
